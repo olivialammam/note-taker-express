0cfad3ab9c7f6b2ecb058c0cd625aafbd33c7415
Olivia Lammam <olivia.l7475@gmail.com>
2022-05-21 21:09:47 -0700

    creating delete

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,11 +2,57 @@ const path = require('path');
 const router = require('express').Router();
 const fs = require('fs');
 
+// GET 
 router.get('/notes', (req, res) => {
 fs.readFile('./db/db.json', 'UTF-8', (err, data) => {
     if (err) throw err;
     res.send(data)
 })
 })
+
+// POST
+router.post('/notes', (req, res) => {
+
+    let note = req.body;
+    note["title"] = req.body.title;
+    note["text"] = req.body.text;
+
+    fs.readFile('./db/db.json', 'utf8', (error, file) => {
+        if (error) throw error;
+
+        const parsedFile = JSON.parse(file);
+        parsedFile.push(note);
+        const newStringifiedFile = JSON.stringify(parsedFile);
+    
+        fs.writeFile('./db/db.json', newStringifiedFile, 'utf8', (err) => {
+            if (err) throw err;
+            console.log("This new note was added to the database!");
+        });
+
+        return res.send(JSON.parse(newStringifiedFile));
+    });    
+});
+
+// DELETE Notes
+// router.delete('/notes/:title', (req, res) => {
+    
+//     fs.readFile('./db/db.json', 'utf8', (error, file) => {
+//         if (error) throw error;
+
+//         let deletedNoteTitle = req.params.title;
+//         const parsedFile = JSON.parse(file);
+//         const newParsedFile = parsedFile.filter((elem) => elem.title != deletedNoteTitle);
+//         const newStringifiedFile = JSON.stringify(newParsedFile);
+
+//         fs.writeFile('./db/db.json', newStringifiedFile, 'utf8', (err) => {
+//             if (err) throw err;
+//             console.log("This note was deleted!");
+//         });
+        
+//         return res.send(JSON.parse(newStringifiedFile));
+//     });
+// });
+
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const htmlRoutes = require('./routes/htmlRoutes');
 const apiRoutes = require('./routes/apiRoutes');
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001; 
 const app = express();
 
 app.use(express.static('public'));
